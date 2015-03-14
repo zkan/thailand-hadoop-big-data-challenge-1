eb5c028add075dce22c3b6c2635e8b62d8f4ab77
prontokan <kan@prontomarketing.com>
2015-03-14 09:48:36 +0700

    list airlines with the number of flights being used

--- code/top_airlines_reducer.py
+++ code/top_airlines_reducer.py
@@ -7,6 +7,24 @@ import sys
 reader = csv.reader(sys.stdin, delimiter=',')
 writer = csv.writer(sys.stdout, delimiter=',')
 
+old_key = None
+count = 0
+
 for line in reader:
     if len(line) > 0:
-        writer.writerow(line)
+        carrier_id = line[0]
+        this_key = carrier_id
+
+        if old_key and old_key != this_key:
+            results = [old_key, count]
+            writer.writerow(results)
+
+            count = 0
+
+        old_key = this_key
+        count += 1
+
+
+if old_key != None:
+    results = [old_key, count]
+    writer.writerow(results)
